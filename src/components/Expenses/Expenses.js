793c951f09adb72f84a3f74d5ffe497975dcc9b9
baso <bartlomiej.sopecki@mobica.com>
2022-08-31 12:03:06 +0200

    filter expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React ,{useState}from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = ({ items }) => {
+  const [filteredYear,setFilteredYear] = useState('2020')
+
+  const filterChangeHandler = (selectedYear)=> {
+    setFilteredYear(selectedYear)
+  }
+
+  const filteredExpenses = items.filter(expense=> {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
   return (
     <Card className="expenses">
-      {items.map((expenseItem) => {
+      <ExpensesFilter  selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      {filteredExpenses.map((expenseItem) => {
         return (
           <ExpenseItem
             key={expenseItem.id}
